e4b4722747a82d40661a2486939c67c4df1fe03d
Khalid Graine <khalidgraine1@gmail.com>
2023-11-28 16:57:17 +0100

    create the delete function

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,49 @@
 import { useState } from "react";
 
 function App() {
+  const [opration, setOperation] = useState('')
+
   let type = (e) => {
-   console.log(e.target.textContent.trim())
+   let v = e.target.textContent.trim();
+   setOperation(`${opration}${v}`)
+  }
+  let deleteAll = () => {
+    setOperation('')
   }
   return (
     <>
       <h3 className="text-center">calculator</h3>
-      <div className="">
-        <form >
-          <input type="text" className="border w-full my-1" />
-        </form>
+      <div className="border px-2">
+      <p>{opration} </p>
       </div>
       <main className=" grid grid-cols-4">
         <div className="col-span-4 grid grid-cols-4">
-          <div className="box">(</div>
-          <div className="box">)</div>
+          <div  onClick={(e) => type(e)} className="box">(</div>
+          <div  onClick={(e) => type(e)} className="box">)</div>
           <div className="box">C</div>
-          <div className="box">delete</div>
+          <div onClick={() => deleteAll()} className="box">delete</div>
         </div>
         <div className="col-span-3 grid  grid-cols-3">
-          <div className="box">9</div>
-          <div className="box">8</div>
-          <div className="box">7</div>
-          <div className="box">6</div>
-          <div className="box">5</div>
-          <div className="box">4</div>
-          <div className="box">3</div>
-          <div className="box">2</div>
-          <div className="box">1</div>
+          <div onClick={(e) => type(e)} onClick={(e) => type(e)} className="box">9</div>
+          <div onClick={(e) => type(e)} onClick={(e) => type(e)} className="box">8</div>
+          <div onClick={(e) => type(e)} onClick={(e) => type(e)} className="box">7</div>
+          <div onClick={(e) => type(e)} onClick={(e) => type(e)} className="box">6</div>
+          <div onClick={(e) => type(e)} onClick={(e) => type(e)} className="box">5</div>
+          <div onClick={(e) => type(e)} onClick={(e) => type(e)} className="box">4</div>
+          <div onClick={(e) => type(e)} onClick={(e) => type(e)} className="box">3</div>
+          <div onClick={(e) => type(e)} onClick={(e) => type(e)} className="box">2</div>
+          <div onClick={(e) => type(e)} onClick={(e) => type(e)} className="box">1</div>
         </div>
         <div>
-          <div className="box">/</div>
-          <div className="box">*</div>
-          <div className="box">-</div>
+          <div onClick={(e) => type(e)} className="box">/</div>
+          <div onClick={(e) => type(e)} className="box">*</div>
+          <div onClick={(e) => type(e)} className="box">-</div>
         </div>
         <div className=" col-span-4 grid grid-cols-4">
-          <div className="box">0</div>
-          <div className="box">.</div>
+          <div onClick={(e) => type(e)} className="box">0</div>
+          <div onClick={(e) => type(e)} className="box">.</div>
           <div className="box">=</div>
-          <div onClick={(e) => type(e)} className="box">+</div>
+          <div onClick={(e) => type(e)}  className="box">+</div>
         </div>
       </main>
     </>
