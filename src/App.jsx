1ca4cc7d2748460439e372afa4a73650aeab2a64
Khalid Graine <khalidgraine1@gmail.com>
2023-11-29 10:40:41 +0100

    put calculator buttons and function inside the component

--- src/App.jsx
+++ src/App.jsx
@@ -50,37 +50,7 @@ function App() {
       <div className="border px-2  h-10 flex justify-end items-center">
      {expression} 
       </div>
-      <main className=" grid grid-cols-4">
-        <div className="col-span-4 grid grid-cols-4">
-          <div  onClick={(e) => type(e)} className="box">(</div>
-          <div  onClick={(e) => type(e)} className="box">)</div>
-          <div onClick={() => removeLastNumber()} className="box">C</div>
-          <div onClick={() => deleteAll()} className="box">delete</div>
-        </div>
-        <div className="col-span-3 grid  grid-cols-3">
-          <div  onClick={(e) => type(e)} className="box">9</div>
-          <div  onClick={(e) => type(e)} className="box">8</div>
-          <div  onClick={(e) => type(e)} className="box">7</div>
-          <div  onClick={(e) => type(e)} className="box">6</div>
-          <div  onClick={(e) => type(e)} className="box">5</div>
-          <div  onClick={(e) => type(e)} className="box">4</div>
-          <div  onClick={(e) => type(e)} className="box">3</div>
-          <div  onClick={(e) => type(e)} className="box">2</div>
-          <div  onClick={(e) => type(e)} className="box">1</div>
-        </div>
-        <div>
-          <div onClick={(e) => type(e)} className="box">/</div>
-          <div onClick={(e) => type(e)} className="box">*</div>
-          <div onClick={(e) => type(e)} className="box">-</div>
-        </div>
-        <div className=" col-span-4 grid grid-cols-4">
-          <div onClick={(e) => type(e)} className="box">0</div>
-          <div onClick={(e) => type(e)} className="box">.</div>
-          <div onClick={() => calculateExpression()} className="box">=</div>
-          <div onClick={(e) => type(e)}  className="box">+</div>
-        </div>
-      </main>
-      <Calculator />
+      <Calculator {...{type,removeLastNumber,deleteAll,calculateExpression}} />
       <Histories histories={histories} />
       </div>
     </>
--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,8 +1,37 @@
 import React from 'react'
 
-const Calculator = () => {
+const Calculator = ({type,removeLastNumber,deleteAll,calculateExpression}) => {
   return (
-    <div>Calculator</div>
+    <main className=" grid grid-cols-4">
+        <div className="col-span-4 grid grid-cols-4">
+          <div  onClick={(e) => type(e)} className="box">(</div>
+          <div  onClick={(e) => type(e)} className="box">)</div>
+          <div onClick={() => removeLastNumber()} className="box">C</div>
+          <div onClick={() => deleteAll()} className="box">delete</div>
+        </div>
+        <div className="col-span-3 grid  grid-cols-3">
+          <div  onClick={(e) => type(e)} className="box">9</div>
+          <div  onClick={(e) => type(e)} className="box">8</div>
+          <div  onClick={(e) => type(e)} className="box">7</div>
+          <div  onClick={(e) => type(e)} className="box">6</div>
+          <div  onClick={(e) => type(e)} className="box">5</div>
+          <div  onClick={(e) => type(e)} className="box">4</div>
+          <div  onClick={(e) => type(e)} className="box">3</div>
+          <div  onClick={(e) => type(e)} className="box">2</div>
+          <div  onClick={(e) => type(e)} className="box">1</div>
+        </div>
+        <div>
+          <div onClick={(e) => type(e)} className="box">/</div>
+          <div onClick={(e) => type(e)} className="box">*</div>
+          <div onClick={(e) => type(e)} className="box">-</div>
+        </div>
+        <div className=" col-span-4 grid grid-cols-4">
+          <div onClick={(e) => type(e)} className="box">0</div>
+          <div onClick={(e) => type(e)} className="box">.</div>
+          <div onClick={() => calculateExpression()} className="box">=</div>
+          <div onClick={(e) => type(e)}  className="box">+</div>
+        </div>
+      </main>
   )
 }
 
