498098ee84d78ba145f1d3b0c01053d23241e502
Khalid Graine <khalidgraine1@gmail.com>
2023-11-30 09:47:07 +0100

    style the Title

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Title = ({text}) => {
   return (
-    <h3 className="text-center font-semibold mt-4">{text}</h3>
+    <h3 className=" font-bold text-gray-800 mt-6 mb-1 uppercase border-b-2   w-fit mx-auto border-yellow-300">{text}</h3>
   )
 }
 
